e5d0f30b402b2184735a4979e59463573094f7d7
saltlakecity <pazhilaya.chichimra@mail.ru>
2025-03-10 14:57:30 +0300

    commit 1

---
+++ client/ui-react/src/Components/FormsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './FormsList.css'
+function FormsList() {
+  return (
+    <div>
+      <div className="classname"></div>
+    </div>
+  )
+}
+
+export default FormsList
--- server/index.js
+++ server/index.js
@@ -2,4 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const { Pool } = require('pg');
 dotenv.config();
+
 const app = express();
+const PORT = 5000
+app.listen(PORT, () => console.log(`server started on port ${5000}`))
