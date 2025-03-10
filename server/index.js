b4071b4e46bf87f4205e3de0e9b6903b9e4232ca
saltlakecity <pazhilaya.chichimra@mail.ru>
2025-03-10 15:22:37 +0300

    db linked

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv')
 const { Pool } = require('pg');
 dotenv.config();
 
 const app = express();
-const PORT = 5000
-app.listen(PORT, () => console.log(`server started on port ${5000}`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server started on port ${PORT}`))
