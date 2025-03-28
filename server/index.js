92abcf17ea339338340bc0f76415a4e55e0d0c1b
saltlakecity <pazhilaya.chichimra@mail.ru>
2025-03-28 17:11:39 +0300

    db test 4

--- server/index.js
+++ server/index.js
@@ -19,70 +19,7 @@ app.post(`/bot${process.env.TELEGRAM_BOT_TOKEN}`, (req, res) => {
     bot.processUpdate(req.body);
     res.sendStatus(200);
 });
-
-
-// //  Функция для чтения данных из user_statuses.json
-// const readUserStatuses = () => {
-//     try {
-//         const data = fs.readFileSync('user_statuses.json', 'utf8');
-//         return JSON.parse(data);
-//     } catch (err) {
-//         console.error('Ошибка чтения файла user_statuses.json:', err);
-//         return {}; //  Возвращаем пустой объект, если файл не существует или поврежден
-//     }
-// };
-
-//  Функция для записи данных в user_statuses.json
-// const writeUserStatuses = (data) => {
-//     try {
-//         fs.writeFileSync('user_statuses.json', JSON.stringify(data, null, 2), 'utf8');
-//     } catch (err) {
-//         console.error('Ошибка записи в файл user_statuses.json:', err);
-//     }
-// };
-
-
-// //  API для получения статусов опросов пользователя
-// app.get('/api/user-statuses/:userId', (req, res) => {
-//     const userId = req.params.userId;
-//     const userStatuses = readUserStatuses();
-//     //  Возвращаем статусы для конкретного пользователя или пустой объект, если пользователя нет
-//     res.json(userStatuses[userId] || {});
-// });
-
-
-// //  API для обновления статуса опроса пользователя
-// app.post('/api/user-statuses/:userId', (req, res) => {
-//     const userId = req.params.userId;
-//     const { formId, status } = req.body;
-
-//     const userStatuses = readUserStatuses();
-
-//     //  Если пользователя еще нет, создаем запись для него
-//     if (!userStatuses[userId]) {
-//         userStatuses[userId] = {};
-//     }
-
-//     //  Обновляем статус опроса
-//     userStatuses[userId][formId] = status;
-//     writeUserStatuses(userStatuses);
-
-//     res.json({ message: 'Статус успешно обновлен' });
-// });
-
-// app.get('/api/test', (req, res) => {
-//     return res.json({ message: 'backend zaebis class' });
-// });
-
-
-//  PostgreSQL Pool
 const pool = new Pool({
-
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    database: process.env.PGDATABASE,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false //  позже настроить
